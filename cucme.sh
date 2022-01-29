916b7fc361ad4608664969a8fa2178d3d6b3b7f0
n00py <n00py@protonmail.com>
2022-01-28 23:55:53 -0700

    Update cucme.sh

--- cucme.sh
+++ cucme.sh
@@ -1,7 +1,11 @@
-read -p 'Server: ' server
+if [ $# -eq 0 ]
+  then
+    echo "Please provide a hostname or IP Address"
+    exit 1
+fi
 echo 'Please wait just a moment...'
 for x in {a..z}
 	do for y in {a..z}
-		do curl "https://$server:8443/cucm-uds/users?name=$x$y" -k -s | xml_grep userName --text_only 
+		do curl "https://$1:8443/cucm-uds/users?name=$x$y" -k -s | xml_grep userName --text_only 
 	done 
 done | sort -u
